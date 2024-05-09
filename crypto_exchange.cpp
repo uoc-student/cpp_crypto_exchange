4a79e566d5a14f593db0874b5ee29c1f00510477
Sony Dev <sony_avil@protnmail.com>
2024-05-09 12:12:15 +0300

    crypto menu done

---
+++ crypto_exchange.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+int main() {
+    // printing a menu for the exchange
+    // 1. Printing help
+    std::cout << "1: Print help" << std::endl;
+    // 2. Print exchange stats
+    std::cout << "2. Printing exchange stats" << std::endl;
+    // 3. Make an offer
+    std::cout << "3. Make an offer" << std::endl;
+    // 4. Make a bid
+    std::cout << "4. Make a bid" << std::endl;
+    // 5. Print wallet
+    std::cout << "Print wallet" << std::endl;
+    // 6. Continue
+    std::cout << "Continue" << std::endl;
+
+    return 0;
+}
