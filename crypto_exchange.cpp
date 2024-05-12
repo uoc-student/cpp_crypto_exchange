be6a4ab172f1b6aeaa4367fa731866b5def69ab0
Sony Dev <sony_avil@protnmail.com>
2024-05-12 17:33:18 +0300

    user menu

--- crypto_exchange.cpp
+++ crypto_exchange.cpp
@@ -6,7 +6,7 @@ int main() {
     // 1. Printing help
     std::cout << "1: Print help" << std::endl;
     // 2. Print exchange stats
-    std::cout << "2. Printing exchange stats" << std::endl;
+    std::cout << "2. Print exchange stats" << std::endl;
     // 3. Make an offer
     std::cout << "3. Make an offer" << std::endl;
     // 4. Make a bid
@@ -16,5 +16,37 @@ int main() {
     // 6. Continue
     std::cout << "6. Continue" << std::endl;
 
-    return 0;
+    std::cout << "=========================" << std::endl;
+
+    int userChoice;
+
+    while (true) {
+        std::cout << "Choose an option from 1 to 6: " << std::endl;
+        std::cin >> userChoice;
+        std::cout << "You chose option: " << userChoice << std::endl;
+
+
+        switch(userChoice) {
+        case 1:
+            std::cout << "Printing help..." << std::endl;
+            break;
+        case 2:
+            std::cout << "Printing exchange stats..." << std::endl;
+            break;
+        case 3:
+            std::cout << "Plase, make an offer..." << std::endl;
+            break;
+        case 4:
+            std::cout << "Please, make a bid..." << std::endl;
+            break;
+        case 5:
+            std::cout << "Printing wallet..." << std::endl;
+            break;
+        case 6:
+            std::cout << "Executing..." << std::endl;
+            break;
+        default:
+            std::cout << "Bad input, please chose a valid option!" << std::endl;
+        }
+    }
 }
